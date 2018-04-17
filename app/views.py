e5cfab5dd4e954c274d78d96d9156e025c1d9c64
anilkumarggk <anilkumarggk@gmail.com>
2018-04-17 21:08:46 +0530

    setup basic routing and index_view

--- app/views.py
+++ app/views.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+
 # Create your views here.
+def index_view(request):
+    if request.method == 'GET':
+        return render(request, 'index.html')
