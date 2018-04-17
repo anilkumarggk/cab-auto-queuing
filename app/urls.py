cb399032b006d927400f016c95b2834302a55b27
anilkumarggk <anilkumarggk@gmail.com>
2018-04-18 00:21:49 +0530

    Request ride API added

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+from .views import UserViewSet
 
 urlpatterns = [
+    url(r'^request_ride/$', UserViewSet.as_view({'post': 'create'})),
     # url(r'^$', app_urls),
 ]
--- app/views.py
+++ app/views.py
@@ -2,6 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import viewsets, permissions, status
+from .serializers import RideSerializer
+from .models import Ride
 
 
 # Create your views here.
@@ -23,3 +27,29 @@ def driverapp_view(request):
 def dashboard_view(request):
     if request.method == 'GET':
         return render(request, 'dashboard.html')
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RideSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            user_id = request.POST['user_id']
+        except:
+            return JsonResponse({"message": "Invalid request"}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            requested = Ride.objects.filter(user=user_id, status__in=['waiting', 'ongoing']).exists()
+            if requested:
+                return JsonResponse({"message": "Request already registered"}, status=status.HTTP_202_ACCEPTED)
+            else:
+                new_ride = self.serializer_class(data={'user': user_id})
+                if new_ride.is_valid():
+                    new_ride.save()
+                    return JsonResponse(new_ride.data, status=status.HTTP_200_OK)
+                else:
+                    return JsonResponse({"message": "Invalid data", "errors": new_ride.errors},
+                                        status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return JsonResponse({"message": "Something went wrong while requesting your ride"},
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
