7ec424e5e6911e71e6ff301feeab3e394d0884e7
anilkumarggk <anilkumarggk@gmail.com>
2018-04-17 23:16:50 +0530

    Added ride model

--- app/models.py
+++ app/models.py
@@ -3,4 +3,23 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+RIDE_STATUS_CHOICES = (
+    ('waiting', 'Waiting'),
+    ('ongoing', 'Ongoing'),
+    ('complete', 'Complete')
+)
+
+AVAILABLE_DRIVER_CHOICES = (
+    ('driver1', 'Driver 1'),
+    ('driver2', 'Driver 2'),
+    ('driver3', 'Driver 3'),
+    ('driver4', 'Driver 4'),
+    ('driver5', 'Driver 5')
+)
+
+
+class Ride(models.Model):
+    user = models.IntegerField(null=False, default=0)
+    status = models.CharField(choices=RIDE_STATUS_CHOICES, default='waiting', max_length=64)
+    pickup_driver = models.CharField(choices=AVAILABLE_DRIVER_CHOICES, null=True, max_length=64)
